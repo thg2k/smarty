7fe08ca4f2c957ec9577ccac51885128a6b89c91
Giovanni Giacobbi <giovanni@giacobbi.net>
2021-12-13 16:57:59 +0100

    Cleaned up version 2.6.27

--- libs/Smarty.class.php
+++ libs/Smarty.class.php
@@ -27,7 +27,7 @@
  * @author Monte Ohrt <monte at ohrt dot com>
  * @author Andrei Zmievski <andrei@php.net>
  * @package Smarty
- * @version 2.6.25-dev
+ * @version 2.6.27
  */
 
 /* $Id$ */
@@ -465,7 +465,7 @@ class Smarty
      *
      * @var string
      */
-    var $_version              = '2.6.25-dev';
+    var $_version              = '2.6.27';
 
     /**
      * current template inclusion depth
